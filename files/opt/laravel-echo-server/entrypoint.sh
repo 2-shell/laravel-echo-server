8707b0b3760c202c31ac35f8525b7d8495e2a9ab
Tuschl <software@2-shell.de>
2020-04-03 17:24:51 +0200

    Make entrypoint script more robust

--- files/opt/laravel-echo-server/entrypoint.sh
+++ files/opt/laravel-echo-server/entrypoint.sh
@@ -5,10 +5,12 @@ if ! [ -e laravel-echo-server.json ]; then
   cp laravel-echo-server.example.json laravel-echo-server.json
 fi
 
-sed -i -e "s%{{ ECHO_CLIENTS }}%$ECHO_CLIENTS%g" laravel-echo-server.json
-sed -i -e "s%{{ ECHO_ALLOW_CORS }}%$ECHO_ALLOW_CORS%g" laravel-echo-server.json
-sed -i -e "s%{{ ECHO_ALLOW_ORIGIN }}%$ECHO_ALLOW_ORIGIN%g" laravel-echo-server.json
-sed -i -e "s%{{ ECHO_ALLOW_METHODS }}%$ECHO_ALLOW_METHODS%g" laravel-echo-server.json
-sed -i -e "s%{{ ECHO_ALLOW_HEADERS }}%$ECHO_ALLOW_HEADERS%g" laravel-echo-server.json
+if [ -w laravel-echo-server.json ]; then
+    sed -i -e "s%{{ ECHO_CLIENTS }}%$ECHO_CLIENTS%g" laravel-echo-server.json
+    sed -i -e "s%{{ ECHO_ALLOW_CORS }}%$ECHO_ALLOW_CORS%g" laravel-echo-server.json
+    sed -i -e "s%{{ ECHO_ALLOW_ORIGIN }}%$ECHO_ALLOW_ORIGIN%g" laravel-echo-server.json
+    sed -i -e "s%{{ ECHO_ALLOW_METHODS }}%$ECHO_ALLOW_METHODS%g" laravel-echo-server.json
+    sed -i -e "s%{{ ECHO_ALLOW_HEADERS }}%$ECHO_ALLOW_HEADERS%g" laravel-echo-server.json
+fi
 
-$@
+exec "$@"
